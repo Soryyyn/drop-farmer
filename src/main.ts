d6ba99d0e5b692b62d5c9a7317a0270016c9aafc
Soryn Bächli <soryn.baechli@gmail.com>
2022-07-10 15:48:38 +0200

    ✨ added reading, saving and creating default settings file

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import Settings from "./settings";
+
+const appSettings = new Settings();
+
+console.log(appSettings.getCurrentSettings());
---
+++ src/settings.ts
@@ -0,0 +1,63 @@
+import { existsSync, readFileSync, writeFile } from "fs";
+import { join } from "path";
+
+/**
+ * Class for managing the settings file and the system around it.
+ */
+export default class Settings {
+    // TODO: change to app path, when refactored
+    private _settingsFilePath: string = join(__dirname, "..", "settings.json");
+    private _defaultSettings = {
+        headless: false,
+        autoRestart: false,
+        launchOnStartup: false,
+        players: [],
+        farms: []
+    };
+    private _settings: Object;
+
+    /**
+     * Read the settings file and save it in the class on class init.
+     */
+    constructor() {
+        this._settings = this.readSettingsFile();
+    }
+
+    /**
+     * Get the current settings.
+     * Also re-reads the settings file to get actual **current** settings.
+     */
+    public getCurrentSettings() {
+        if (this._settings) {
+            this._settings = this.readSettingsFile();
+            return this._settings;
+        }
+    }
+
+    /**
+     * Read the settings file from the set filepath.
+     * Also checks if the settings file exists, if not it creates a new default one.
+     */
+    public readSettingsFile(): any {
+        if (!existsSync(this._settingsFilePath)) {
+            this.createSettingsFile();
+        }
+
+        // TODO: re-work error throwing to make app not crash, when reading settings file fails? (fallback to default)
+        try {
+            let fileData: string = readFileSync(this._settingsFilePath).toString();
+            return JSON.parse(fileData);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    /**
+     * Write a new default settigns file into the application directory.
+     */
+    private createSettingsFile() {
+        writeFile(this._settingsFilePath, JSON.stringify(this._defaultSettings, null, 4), (err) => {
+            if (err) throw err;
+        });
+    }
+}
