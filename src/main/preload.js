6a935c027d73447cd645165f4a018bf9d0584a30
Soryn Bächli <soryn.baechli@gmail.com>
2022-07-31 15:26:03 +0200

    Better comments on preload file

--- src/main/preload.js
+++ src/main/preload.js
@@ -4,12 +4,31 @@ import { Channels } from "./common/channels";
 
 /**
  * Expose functionality from main to renderer process.
- * Channels for IPC are in the channels object.
+ *
+ * Use the the context bridge via "window.api" and then the exposed methods / objects.
+ * Ex. window.api.callMain()
  */
 contextBridge.exposeInMainWorld("api", {
+    /**
+     * The channels for ipc communication.
+     */
     channels: {
         ...Channels
     },
+    /**
+     * Call the main process to execute functions on main process and send back.
+     *
+     * @param {Channels} channel The channel to call on main.
+     * @param  {...any} args Arguments / data to send to main.
+     * @returns void
+     */
     callMain: (channel, ...args) => ipcRenderer.callMain(channel, ...args),
+    /**
+     * Reply to the call from main process.
+     *
+     * @param {Channels} channel The channel to answer from main.
+     * @param {any} callback Function/'s to execute when replying to main.
+     * @returns void
+     */
     answerMain: (channel, callback) => ipcRenderer.answerMain(channel, callback => any)
 });
