3a4f5911bfc080b0b54fc75880dd5e3b91142bb2
Soryn Bächli <soryn.baechli@gmail.com>
2022-08-28 16:54:12 +0200

    Now quitting application on fatal error log

--- src/main/logger.ts
+++ src/main/logger.ts
@@ -1,5 +1,6 @@
 import * as color from "ansi-colors";
 import dayjs from "dayjs";
+import { app } from "electron";
 import { existsSync, writeFileSync } from "fs";
 import { join } from "path";
 import { APP_PATH, writeToFile } from "./fileHandling";
@@ -7,12 +8,10 @@ import { APP_PATH, writeToFile } from "./fileHandling";
 const FILE_NAME: string = ".log";
 
 /**
- * Create the ".log" file.
+ * Create the `.log` file.
  *
  * The logfile does *not* get created with the written filehandling methods,
  * because it *needs* to exist before something can be logged.
- *
- * @returns void
  */
 export function initLogger(): void {
     if (existsSync(join(join(APP_PATH), FILE_NAME))) {
@@ -31,25 +30,25 @@ export function initLogger(): void {
  * Creates an appropriate log entry for the log file with timestamp,
  * type and message.
  *
- * @param {"MAIN" | "RENDERER"} process From which process the log is coming from.
+ * @param {"MAIN" | "RENDERER"} location From which location the log is coming from.
  * @param {"FATAL" | "ERROR" | "WARN" | "INFO" | "DEBUG"} type Type of log entry to make.
  * @param {string} message Message to include in log entry.
  * @returns {string} Prepared log entry.
  */
-function createLogEntry(process: "MAIN" | "RENDERER", type: "FATAL" | "ERROR" | "WARN" | "INFO" | "DEBUG", message: string): string {
+function createLogEntry(location: "MAIN" | "RENDERER", type: "FATAL" | "ERROR" | "WARN" | "INFO" | "DEBUG", message: string): string {
     let currentTimeStamp = dayjs().format("YYYY-MM-DD HH:mm:ss.SSS");
-    return `[${type}] (${process}) ${currentTimeStamp} - ${message}\n`;
+    return `[${type}] (${location}) ${currentTimeStamp} - ${message}\n`;
 }
 
 /**
  * Creates an appropriate log entry for the the terminal with colors.
  *
- * @param {"MAIN" | "RENDERER"} process From which process the log is coming from.
+ * @param {"MAIN" | "RENDERER"} location From which location the log is coming from.
  * @param {"FATAL" | "ERROR" | "WARN" | "INFO" | "DEBUG"} type Type of log entry to make.
  * @param {string} message Message to include in log entry.
  * @returns {string} Prepared log entry.
  */
-function createTerminalLogEntry(process: "MAIN" | "RENDERER", type: "FATAL" | "ERROR" | "WARN" | "INFO" | "DEBUG", message: string): string {
+function createTerminalLogEntry(location: "MAIN" | "RENDERER", type: "FATAL" | "ERROR" | "WARN" | "INFO" | "DEBUG", message: string): string {
     let currentTimeStamp = dayjs().format("YYYY-MM-DD HH:mm:ss.SSS");
 
     /**
@@ -67,11 +66,11 @@ function createTerminalLogEntry(process: "MAIN" | "RENDERER", type: "FATAL" | "E
     /**
      * Process color.
      */
-    let processText: string;
-    if (process === "MAIN")
-        processText = color.bold.green(`(${process})`);
+    let locationText: string;
+    if (location === "MAIN")
+        locationText = color.bold.green(`(${location})`);
     else
-        processText = color.bold.magenta(`(${process})`);
+        locationText = color.bold.magenta(`(${location})`);
 
     /**
      * Timestamp color
@@ -86,22 +85,37 @@ function createTerminalLogEntry(process: "MAIN" | "RENDERER", type: "FATAL" | "E
     /**
      * Build the text from blocks.
      */
-    return `${typeText} ${processText} ${timeStampText} - ${messageText}`;
+    return `${typeText} ${locationText} ${timeStampText} - ${messageText}`;
 }
 
 /**
  * Logs (writes) a log entry into the ".log" file in the app dir.
  *
- * @param {"MAIN" | "RENDERER"} process From which process the log is coming from.
+ * @param {"MAIN" | "RENDERER"} location From which location the log is coming from.
  * @param {"FATAL" | "ERROR" | "WARN" | "INFO" | "DEBUG"} type Type of log entry to make.
  * @param {string} message Message to log.
  */
-export function log(process: "MAIN" | "RENDERER", type: "FATAL" | "ERROR" | "WARN" | "INFO" | "DEBUG", message: any): void {
-    let entry = createLogEntry(process, type, message);
-    let terminalEntry = createTerminalLogEntry(process, type, message);
+export function log(location: "MAIN" | "RENDERER", type: "FATAL" | "ERROR" | "WARN" | "INFO" | "DEBUG", message: any): void {
+    let entry: string = createLogEntry(location, type, message);
+    let terminalEntry: string = createTerminalLogEntry(location, type, message);
+
+    /**
+     * If in production environment disable debug-type loggings.
+     */
+    if ((process.env.NODE_ENV === "production") && type === "DEBUG")
+        return;
+
     try {
         console.log(terminalEntry);
         writeToFile(FILE_NAME, `${entry}`, "a");
+
+        /**
+         * After logging, check if the type is "FATAL".
+         * If it is, then soft quit the application.
+         */
+        if (type === "FATAL") {
+            app.quit();
+        }
     } catch (err) {
         console.log(err);
     }
