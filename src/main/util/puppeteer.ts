5eece40aacb06a3590b270a16cbc5f864a1c0083
Soryn Bächli <soryn.baechli@gmail.com>
2022-11-05 10:03:46 +0100

    Added functions to enter iframe and waitfortimeout (got deprecated)

--- src/main/util/puppeteer.ts
+++ src/main/util/puppeteer.ts
@@ -1,5 +1,5 @@
 import { app } from "electron";
-import puppeteer, { Browser } from "puppeteer-core";
+import puppeteer, { Browser, Frame, Page } from "puppeteer-core";
 import { connect, initialize } from "puppeteer-in-electron";
 import { log } from "./logger";
 
@@ -22,4 +22,48 @@ export async function initPuppeteerConnection() {
  */
 export function getBrowserConnection(): Browser {
     return browserConnection;
+}
+
+/**
+ * Manually implemented timeout waiting for use.
+ */
+export function waitForTimeout(timeout: number): Promise<void> {
+    return new Promise((r) => setTimeout(r, timeout));
+}
+
+/**
+ * Enter the iframe to control it.
+ *
+ * @param {string} gameName The name of the game currently running.
+ * @param {Page} controlledPage The upper page which the iframe is on.
+ */
+export function enterIFrame(gameName: string, controlledPage: Page) {
+    return new Promise<any>(async (resolve, reject) => {
+        try {
+            /**
+             * Reload the page if the iframe is not found.
+             */
+            while ((await controlledPage.$("iframe")) == null) {
+                log(
+                    "MAIN",
+                    "DEBUG",
+                    `${gameName}: Page needs to reload because iframe is not loaded`
+                );
+                await controlledPage.reload();
+            }
+
+            /**
+             * Enter the iframe element.
+             */
+            await controlledPage
+                .waitForSelector("iframe")
+                .then(async (iframeHandle) => {
+                    await controlledPage.waitForNetworkIdle();
+                    log("MAIN", "DEBUG", `${gameName}: Endered iframe element`);
+                    resolve(await iframeHandle!.contentFrame());
+                });
+        } catch (err) {
+            reject(err);
+        }
+    });
 }
