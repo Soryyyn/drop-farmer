9f8546a8d98aa313bd5e2eee2d780f8142a1e7c3
Soryn Bächli <soryn.baechli@gmail.com>
2022-12-24 11:55:49 +0100

    Now catching system suspend and resume events

---
+++ src/main/util/powermonitor.ts
@@ -0,0 +1,21 @@
+import { powerMonitor } from 'electron';
+import { EventChannels } from '../common/constants';
+import { emitEvent } from './events';
+import { log } from './logger';
+
+/**
+ * Listen for suspend and resume events, to pause the farms to prevent errors.
+ * If the pc is going to sleep, then no need to farm.
+ *
+ * Maybe add a setting in the future to prevent the pc from going to sleep?
+ */
+
+powerMonitor.on('suspend', () => {
+    log('MAIN', 'INFO', 'PC went to sleep');
+    emitEvent(EventChannels.PCWentToSleep);
+});
+
+powerMonitor.on('resume', () => {
+    log('MAIN', 'INFO', 'PC woke up');
+    emitEvent(EventChannels.PCWokeUp);
+});
