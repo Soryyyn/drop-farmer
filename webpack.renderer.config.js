f743b5d7babcddc5084e954381cb0dadec74980d
Soryn Bächli <soryn.baechli@gmail.com>
2023-01-02 23:33:57 +0100

    Removed unused deps

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -4,7 +4,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-require('dotenv').config();
 
 module.exports = {
     module: {
