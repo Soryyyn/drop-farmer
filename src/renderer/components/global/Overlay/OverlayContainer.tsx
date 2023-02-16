b25a094fe131c2f50e0c53332816d42d5169ee24
Soryn Bächli <soryn.baechli@gmail.com>
2023-02-16 20:49:03 +0100

    Add (mistakingly) removed bounding provider

--- src/renderer/components/global/Overlay/OverlayContainer.tsx
+++ src/renderer/components/global/Overlay/OverlayContainer.tsx
@@ -1,3 +1,4 @@
+import { BoundingContextProvider } from '@renderer/util/contexts';
 import React from 'react';
 
 interface Props {
@@ -6,8 +7,10 @@ interface Props {
 
 export default function OverlayContainer({ children }: Props) {
     return (
-        <div className="relative box-border p-6 h-modal w-modal bg-pepper-600/95 rounded-xl backdrop-blur-2xl shadow-xl shadow-pepper-600/25">
-            {children}
-        </div>
+        <BoundingContextProvider>
+            <div className="relative box-border p-6 h-modal w-modal bg-pepper-600/95 rounded-xl backdrop-blur-2xl shadow-xl shadow-pepper-600/25">
+                {children}
+            </div>
+        </BoundingContextProvider>
     );
 }
