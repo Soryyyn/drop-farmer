c4b63b08ad38410caa89f29026d769d08bfd36f3
Soryn Bächli <soryn.baechli@gmail.com>
2023-03-18 14:05:21 +0100

    Reworking context structure

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,11 @@
+import CloudBackground from '@components/global/CloudBackground';
 import Dragbar from '@components/global/Dragbar';
 import ToastNotifications from '@components/global/ToastNotifications';
-import { AuthContext } from '@contexts/AuthContext';
 import { FarmsContextProvider } from '@contexts/FarmsContext';
-import { InternetConnectionContextProvider } from '@contexts/InternetConnectionContext';
 import { ModalContextProvider } from '@contexts/ModalContext';
 import { SettingsContextProvider } from '@contexts/SettingsContext';
 import { UpdateContextProvider } from '@contexts/UpdateContext';
-import React, { useContext } from 'react';
+import React from 'react';
 import './global.css';
 import Router from './Router';
 
@@ -14,20 +13,19 @@ export default function App() {
     return (
         <>
             <Dragbar />
+            <CloudBackground />
             <ToastNotifications />
-            <InternetConnectionContextProvider>
-                <UpdateContextProvider>
-                    <SettingsContextProvider>
-                        <FarmsContextProvider>
-                            <ModalContextProvider>
-                                <div className="p-8 flex flex-col h-screen">
-                                    <Router />
-                                </div>
-                            </ModalContextProvider>
-                        </FarmsContextProvider>
-                    </SettingsContextProvider>
-                </UpdateContextProvider>
-            </InternetConnectionContextProvider>
+            <UpdateContextProvider>
+                <SettingsContextProvider>
+                    <FarmsContextProvider>
+                        <ModalContextProvider>
+                            <div className="p-8 flex flex-col h-screen">
+                                <Router />
+                            </div>
+                        </ModalContextProvider>
+                    </FarmsContextProvider>
+                </SettingsContextProvider>
+            </UpdateContextProvider>
         </>
     );
 }
---
+++ src/renderer/components/global/CloudBackground/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CloudBackground() {
+    return <div className="absolute -z-10 h-screen w-screen"></div>;
+}
