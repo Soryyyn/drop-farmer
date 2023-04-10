c364a4b2cc0a12b3900a8736b370a36da5eb4818
Soryn Bächli <soryn.baechli@gmail.com>
2023-04-10 22:36:28 +0200

    Removed opacity effect on overlay

--- src/renderer/components/global/Overlay/OverlayContainer.tsx
+++ src/renderer/components/global/Overlay/OverlayContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function OverlayContainer({ children }: Props) {
     return (
-        <div className="relative box-border p-6 h-modal w-modal bg-pepper-600/95 rounded-xl backdrop-blur-2xl shadow-xl shadow-pepper-600/25">
+        <div className="relative box-border p-6 h-modal w-modal bg-pepper-600 rounded-xl shadow-xl shadow-pepper-600/25">
             {children}
         </div>
     );
