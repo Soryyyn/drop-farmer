f1447b019b61112b2cec722f8451a72d4eb53c58
Soryn Bächli <soryn.baechli@gmail.com>
2023-04-25 20:02:36 +0200

    Added a dismiss button to toast notifications

--- src/renderer/components/global/ToastNotifications/index.tsx
+++ src/renderer/components/global/ToastNotifications/index.tsx
@@ -1,27 +1,66 @@
 import { Toast } from '@df-types/toast.types';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useHandleOneWay } from '@hooks/useHandleOneWay';
 import React from 'react';
 import toast, { Toaster } from 'react-hot-toast';
+import Icon from '../Icon';
+
+type ToastContentProps = {
+    label: string;
+    onDismiss: () => void;
+};
+
+function ToastContent({ label, onDismiss }: ToastContentProps) {
+    return (
+        <div className="flex flex-row gap-4 items-center">
+            <p>{label}</p>
+            <button
+                onClick={onDismiss}
+                className="ml-auto aspect-square h-min bg-pepper-400 hover:bg-pepper-500 flex items-center justify-center p-1 rounded-md text-snow-300 active:outline outline-2 outline-offset-2 outline-snow-300/50 transition-all"
+            >
+                <Icon sprite={faXmark} size="1x" />
+            </button>
+        </div>
+    );
+}
 
 export default function ToastNotifications() {
     useHandleOneWay({
         channel: api.channels.toast,
         callback: (event, receivedToast: Toast) => {
             if (receivedToast.type === 'success') {
-                toast.success(receivedToast.textOnSuccess!, {
-                    id: receivedToast.id,
-                    duration: receivedToast.duration
-                });
+                toast.success(
+                    <ToastContent
+                        label={receivedToast.textOnSuccess!}
+                        onDismiss={() => toast.dismiss(receivedToast.id)}
+                    />,
+                    {
+                        id: receivedToast.id,
+                        duration: receivedToast.duration
+                    }
+                );
             } else if (receivedToast.type === 'error') {
-                toast.error(receivedToast.textOnError!, {
-                    id: receivedToast.id,
-                    duration: receivedToast.duration
-                });
+                toast.error(
+                    <ToastContent
+                        label={receivedToast.textOnError!}
+                        onDismiss={() => toast.dismiss(receivedToast.id)}
+                    />,
+                    {
+                        id: receivedToast.id,
+                        duration: receivedToast.duration
+                    }
+                );
             } else if (receivedToast.type === 'loading') {
-                toast.loading(receivedToast.textOnLoading!, {
-                    id: receivedToast.id,
-                    duration: receivedToast.duration
-                });
+                toast.loading(
+                    <ToastContent
+                        label={receivedToast.textOnLoading!}
+                        onDismiss={() => toast.dismiss(receivedToast.id)}
+                    />,
+                    {
+                        id: receivedToast.id,
+                        duration: receivedToast.duration
+                    }
+                );
             }
         }
     });
@@ -32,6 +71,7 @@ export default function ToastNotifications() {
             toastOptions={{
                 style: {
                     minWidth: '450px',
+                    textAlign: 'center',
                     background: 'rgb(21 22 32)',
                     boxShadow:
                         '0 20px 25px -5px rgb(21 22 32 / 0.25), 0 8px 10px -6px rgb(21 22 32 / 0.25)',
